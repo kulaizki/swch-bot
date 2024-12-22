fa8350008d29bdad20313d34809cc624e4ea4622
kulaizki <kulaizke@gmail.com>
2024-12-22 17:51:08 +0800

    feat: add strategy backtester

---
+++ src/backtester.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+class Backtester:
+    def __init__(self, strategy):
+        self.strategy = strategy
+
+    def backtest(self, historical_data):
+        df = pd.DataFrame(historical_data)
+        
+        signals = []
+        for i in range(20, len(df)):  
+            signal = self.strategy.generate_signal(df.iloc[:i])
+            if signal:
+                signals.append(signal)
+        return signals
